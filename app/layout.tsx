89e534146abcebb4103ebe87c14a5e891d3ac733
Frontend Developer <90094867+yusufie@users.noreply.github.com>
2024-02-22 23:19:57 +0300

    Add lang attribute to HTML element

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
+    
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
