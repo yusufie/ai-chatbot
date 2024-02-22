ad7a3b5fc3192aaca096d85b07e3b33045affc72
Frontend Developer <90094867+yusufie@users.noreply.github.com>
2024-02-22 22:21:02 +0300

    Update import path for authOptions in chat share route

--- app/api/chat/share/route.ts
+++ app/api/chat/share/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
-import authOptions from '../../auth/[...nextauth]/options';
+import authOptions from '@/app/api/auth/[...nextauth]/options';
 import Chat from '@/models/chat';
 import connectDB from '@/utils/connectDB'
 
